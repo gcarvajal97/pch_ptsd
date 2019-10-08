73c689ba8ab60ec83bf00ea4e2a97cfb1d0d86c2
Anthony DeCosta <ardecost@asu.edu>
2019-10-08 03:02:01 -0700

    UC0T69: Initial "Hello, World" app in React Native

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hello, Group 16! {"\n\n"}
+        We are up and running! {"\n\n"}
+        ...on Android at least. Ha
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  },
+});
--- README.md
+++
@@ -1,1 +0,0 @@
-# pch_ptsd
