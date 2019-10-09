a493ab1c7c01742c8132e7e72c998c6ec6c544df
leahtbriggs <leahtbriggs@gmail.com>
2019-10-09 10:51:08 -0700

    US20T55: Updated UI Component of Button
    Updated the UI component so the band-aid.png image and the button were inside of the square. Also added in accessibility labels to the components.

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import {
+  StackNavigator,
+} from 'react-navigation';
+import {
   StyleSheet, 
-  Text, 
   View,
   Button,
   Alert,
-  Dimensions,
+  Image,
 } from 'react-native';
+
+//TODO set up MVC so button press goes to new (empty) page
+//TODO fix alignment of button and icon
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.buttonBox}/>
-      <Button
-          title="Learn About Trauma and Injury"
-          onPress={() => Alert.alert('Simple Button pressed')}
-          style={[styles.button]}
+      <View style={styles.buttonBox}>
+        <Image
+          style={styles.image}
+          source={require('./assets/healing_white_48dp_2x.png')}
+          accessibilityLabel="Image of two band-aids criss-crossing"
         />
+        <Button
+          title="Learn About Trauma and Injury"
+          onPress={() => Alert.alert('Go to Trauma and Injury Page')}
+          style={styles.button}
+          accessibilityLabel="Learn more about trauma and injury"
+        />
+      </View>
     </View>
   );
 }
@@ -27,17 +39,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   button: {
-    color: "#5d99c6",
-    width: '90%',
+    color: '#c3fdff',
+    width: '20%',
   },
   buttonBox: {
+    height: '30%',
     width: '90%',
-    height: '20%',
-    backgroundColor: '#90caf9',
-    borderColor: '#c4110e',
+    backgroundColor: '#64b5f6',
+    borderColor: '#1e88e5',
     borderRadius:10,
+  },
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    height: '40%',
+    aspectRatio: 1,
   }
 });
