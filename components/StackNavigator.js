430a9dad0e0abb734d0d3d1e88ad020c359f1f75
leahtbriggs <leahtbriggs@gmail.com>
2019-10-18 09:27:15 -0700

    US20T90: Learn More button links to Learn More Screen
    On the main menu the "Learn More" button now links to the "Learn More" screen.

--- components/StackNavigator.js
+++ components/StackNavigator.js
@@ -4,12 +4,14 @@ import {createAppContainer} from 'react-navigation';
 import MainScreen from '../screens/MainScreen.js';
 import TestScreen from '../screens/TestScreen.js';
 import NavigationService from './NavigationService';
+import LearnMoreScreen from '../screens/LearnMore.js';
 
 // StackNavigator: This navigator controls the flow from the main screen to other screens and back (Stack)
 // This is called by the DrawerNavigator, so it is sort of nested inside
 const NavigationBar = createStackNavigator({
     Home: MainScreen,
-    Test: TestScreen
+    Test: TestScreen,
+    LearnMore: LearnMoreScreen,
   });
   
   // New in this version of React Native, must be created and reference/returned
