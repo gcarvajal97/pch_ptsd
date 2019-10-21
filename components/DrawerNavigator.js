194a05a312f6f11ff8c01f8db29f0f22f91f6afb
Anthony DeCosta <ardecost@asu.edu>
2019-10-20 22:18:50 -0700

    US0T103: Drawer UI displays correct screens now

--- components/DrawerNavigator.js
+++ components/DrawerNavigator.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createDrawerNavigator} from 'react-navigation-drawer';
+
 import StackNavigator from './StackNavigator';
 import NavigationService from './NavigationService';
+import resourceScreen from '../drawerScreens/resourceScreen';
+import glossaryScreen from '../drawerScreens/glossaryScreen';
+
 
 // DrawerNavigator: user opens using button on the top left of the header
 // Drawer slides out from the side and contains components listed below
 // Currently points to the StackNavigator, which points to the MainScreen and TestScreen
 const DrawerNav = createDrawerNavigator({
     Home: StackNavigator,
+    Resources: resourceScreen,
+    Glossary: glossaryScreen
 });
 
 // New in this version of React Native, must be referenced/returned
