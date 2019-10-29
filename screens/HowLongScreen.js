568c677b1445e27f46be02e695de080aeb1d6c4e
leahtbriggs <leahtbriggs@gmail.com>
2019-10-29 15:16:00 -0700

    US92T129: Updated some UI Elements
    Updated some small UI elements.

--- screens/HowLongScreen.js
+++ screens/HowLongScreen.js
@@ -8,7 +8,7 @@ class HowLongScreen extends Component {
 
     // Gives the NavBar a title for this specific screen
     static navigationOptions = {
-        headerTitle: 'How Long Do Reactions Last'
+        headerTitle: 'Learn More'
     }
 
     render() {
@@ -34,29 +34,24 @@ class HowLongScreen extends Component {
                 </Text>
                 <ScrollView>
                     <Text style={styles.paragraph}>
-                    {"\n"}
-                    {'\t'} For many children and their parents early traumatic stress reactions get better over the first month. 
+                    For many children and their parents early traumatic stress reactions get better over the first month. 
                     But about 1 in 6 still have traumatic stress reactions that bother them, even 6 months after an injury. 
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraph}>
-                    {'\t'} Nearly everyone feels upset or anxious early on. 
+                    Nearly everyone feels upset or anxious early on. 
                     Over the next few weeks and months, these reactions usually get weaker and then go away. 
                     Even children recovering from serious injuries start to develop ways to cope.
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraph}>
-                    {'\t'} But for some people, these early reactions do not seem to get better as the months go by, or they may get worse. 
+                    But for some people, these early reactions do not seem to get better as the months go by, or they may get worse. 
                     Even if the physical injury has healed, they may not have recovered from the trauma. 
                     The intensity and length of the traumatic stress reactions is not related to how bad the physical injury was.
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraphTitle}>
                     Why be concerned?
                     </Text>
                     <Text style={styles.paragraph}>
                     When traumatic stress reactions last for weeks or months, they can get in the way of getting back to normal and feeling like yourself again.
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraphTitle}>
                     For children and teens, this can affect school, home, and play.
@@ -65,39 +60,31 @@ class HowLongScreen extends Component {
                     <Text style={styles.bullet}>- Family relationships and friendships might suffer.</Text>
                     <Text style={styles.bullet}>- Children might stop doing things that they enjoy, or stop trying new things.</Text>
                     <Text style={styles.paragraphTitle}>
-                    {"\n"}
                     What is posttraumatic stress disorder, or 'PTSD'?
                     </Text>
                     <Text style={styles.paragraph}>
                     Posttraumatic stress disorder (PTSD) is the name given to traumatic stress reactions that:
-                    {"\n"}
                     </Text>
                     <Text style={styles.bullet}>- Are so severe that they get in the way of normal life and</Text>
                     <Text style={styles.bullet}>- Last for more than one month.</Text>
                     <Text style={styles.paragraph}>
-                    {"\n"}
                     It is important to know if your child or someone else in your family develops posttraumatic stress disorder from the accident or injury. It is much more common than people realize.
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraph}>
                     Up to 1 in 10 people develop posttraumatic stress disorder at some point in their lives.
-                    {"\n"}
                     </Text>
-                    <Text style={{fontWeight: 'bold', fontSize:20, alignSelf:'stretch', textAlign:'left',marginLeft:5, marginTop:2, marginBottom:1}}>
+                    <Text style={{fontWeight: 'bold', fontSize:16, alignSelf:'stretch', textAlign:'left',marginLeft:5, marginTop:4, marginBottom:3}}>
                     After an injury, posttraumatic stress disorder can:
                     </Text>
                     <Text style={styles.bullet}>- Get in the way of physical recovery</Text>
                     <Text style={styles.bullet}>- Contribute to new medical problems; or</Text>
                     <Text style={styles.bullet}>- Just make it harder to enjoy life.</Text>
                     <Text style={styles.link} onPress={()=> Linking.openURL('https://medlineplus.gov/ency/article/000925.htm')}>
-                    {"\n"}
                     Click here for more information on PTSD.
-                    {"\n"}
                     </Text>
                     <Text style={styles.paragraph}>
                     The good news is that very good treatments for PTSD are available. 
                     Unfortunately, less than half of people with traumatic stress symptoms talk to anyone about their problems or get any professional help, even when their symptoms go on for a long time.
-                    {"\n"}
                     </Text>
                 </ScrollView>
         </View>
@@ -136,7 +123,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold', 
         fontSize: 14, 
         textAlign: 'center', 
-        alignSelf: 'stretch'
+        alignSelf: 'stretch',
+        marginBottom: 3,
     },
     paragraphTitle: {
         fontWeight: 'bold',
@@ -144,8 +132,8 @@ const styles = StyleSheet.create({
         alignSelf:'stretch',
         textAlign:'left',
         marginLeft:5,
-        marginTop:2,
-        marginBottom:1,
+        marginTop:4,
+        marginBottom:3,
         color:'#2089DC'
     },
     bullet: {
@@ -160,7 +148,8 @@ const styles = StyleSheet.create({
         alignSelf:"center", 
         paddingVertical:1, 
         paddingHorizontal:5,
-        fontSize: 14
+        fontSize: 14,
+        marginTop: 5,
     },
     link: {
         alignSelf:"center", 
@@ -168,6 +157,8 @@ const styles = StyleSheet.create({
         paddingHorizontal:5,
         fontSize: 16,
         color:'#2089DC',
-        textDecorationLine: 'underline'
+        textDecorationLine: 'underline',
+        marginTop: 4,
+        marginTop: 3,
     }
 });
