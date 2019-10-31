8461d0d350c9611a79a9ccba5820fa0044002830
Anthony DeCosta <ardecost@asu.edu>
2019-10-31 15:20:12 -0700

    US91T126: Styled to match look and feel

--- learnMoreScreens/ReactionsToInjury.js
+++ learnMoreScreens/ReactionsToInjury.js
@@ -30,30 +30,29 @@ class ReactionsToInjury extends Component {
                 <ScrollView>
                     <Text style={styles.paragraph}>
                     One of the most important things you can do to help your child is to recognize your child’s reactions to injury. 
-                    Many reactions are normal and common; some are more worrisome. The first step in helping your child recover is to 
-                    learn what reactions to look for and what you can do to help your child respond in a healthy way.
-                    </Text>
-                    <Text style={{fontWeight: 'bold', fontSize:22, alignSelf:'stretch', textAlign:'left',marginLeft:5, marginTop:2, marginBottom:1, color:'#2089DC'}}>
-                    Bold Content
-                    </Text>
-                    <Text style={styles.bullet}>bullet1</Text>
-                    <Text style={styles.bullet}>bullet2</Text>
-                    <Text style={styles.bullet}>bullet3</Text>
-
-                    <Text style={styles.paragraph}>
-                    In the first few days after an injury, common child trauma symptoms are:
-                    Nearly all children feel upset, jumpy, or worried at times.
-                    Parents and other family members can have similar reactions.
-                    Nearly everyone feels symptoms of stress; Headache, tense muscles, knots in your stomach, sweaty palms. Also feeling that life is a bit out of control.
-                    Parents and children may argue more.
-                    Children may be more clingy or have trouble sleeping.
+                    Many reactions are normal and common; some are more worrisome. 
                     </Text>
                     <Text style={styles.paragraph}>
-                    Some people even notice positive aspects in their reactions to injury. They may:
-                    Feel thankful and relieved that nothing worse happened
-                    Resolve to do things more safely in the future
+                    The first step in helping your child recover is to learn what reactions to look for and what you can do to help 
+                    your child respond in a healthy way.
+                    </Text>
+                    <Text style={{fontWeight: 'bold', fontSize:20, alignSelf:'stretch', textAlign:'left',marginLeft:5, marginTop:2, marginBottom:8, color:'#2089DC'}}>
+                    In the first few days after an injury, these are the common child trauma symptoms:
+                    </Text>
+                    <Text style={styles.bullet}>Nearly all children feel upset, jumpy, or worried at times.</Text>
+                    <Text style={styles.bullet}>Parents and other family members can have similar reactions.</Text>
+                    <Text style={styles.bullet}>Nearly everyone feels symptoms of stress; Headache, tense muscles, knots in your stomach, 
+                                                sweaty palms, and the feeling that life is a bit out of control.</Text>
+                    <Text style={styles.bullet}>Parents and children may argue more.</Text>
+                    <Text style={styles.bulletLast}>Children may be more clingy or have trouble sleeping.</Text>
+                    <Text style={{fontWeight: 'bold', fontSize:20, alignSelf:'stretch', textAlign:'left',marginLeft:5, marginTop:2, marginBottom:8, color:'#2089DC'}}>
+                    You may even notice some positive reactions to their injury:
+                    </Text>
+                    <Text style={styles.bullet}>Feeling thankful and relieved that nothing worse happened</Text>
+                    <Text style={styles.bulletLast}>A resolve to do things more safely in the future</Text>
+                    <Text style={styles.paragraph}>
                     When an accident or injury causes overwhelming feelings of fear, helplessness, and horror, 
-                    it can lead to more than just everyday stress reactions -- it can lead to traumatic stress.
+                    it can lead to more than just everyday stress reactions. It can lead to <Text style={{fontWeight:'bold'}}>traumatic stress.</Text>
                     </Text>        
                 </ScrollView>
             </View>
@@ -80,6 +79,15 @@ const styles = StyleSheet.create({
     },
     bullet: {
         marginVertical:1, 
+        marginHorizontal:18, 
+        textAlign:'left', 
+        alignSelf:'stretch', 
+        fontWeight:'bold',
+        fontSize:14
+    },
+    bulletLast: {
+        marginTop:1,
+        marginBottom:6, 
         marginHorizontal:18, 
         textAlign:'left', 
         alignSelf:'stretch', 
