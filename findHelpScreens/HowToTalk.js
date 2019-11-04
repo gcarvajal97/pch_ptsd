3dbf7a70276558e8f6f6435f121a607369447330
leahtbriggs <leahtbriggs@gmail.com>
2019-11-04 07:19:51 -0800

    US32T150: Import and Embed Video Content
    Imported and embedded the video content from after the injury website.

--- findHelpScreens/HowToTalk.js
+++ findHelpScreens/HowToTalk.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions} from "react-native";
+import { Video } from 'expo-av';
+import { Linking } from "expo";
+import NavigationService from '../components/NavigationService';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,10 +20,20 @@ class HowToTalk extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-                <Text>
-                    This is the How To Talk screen{'\n'}
-                </Text>
+        <View style={styles.container}>
+            <Text>
+                How To Talk With Your Child
+            </Text>
+            <Video
+                    source={require('../assets/videos/vidHowToTalk.mp4')}
+                    rate={1.0}
+                    volume={1.0}
+                    resizeMode={Video.RESIZE_MODE_STRETCH}
+                    resizeMode={Video.RESIZE_MODE_COVER}
+                    shouldPlay
+                    useNativeControls
+                    style={{ height: 204, width: '100%', maxHeight: 254}}
+              />
         </View>
     );
   }
