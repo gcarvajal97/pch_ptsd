a3d0eb5989c056f08ee51f543fea3234922234d7
Anthony DeCosta <ardecost@asu.edu>
2019-11-05 18:17:33 -0800

    US17T158: Page linked to list and app navigator

--- screens/FindHelpList.js
+++ screens/FindHelpList.js
@@ -8,7 +8,7 @@ import NavigationService from '../components/NavigationService';
 const listNames = [
     { name: 'Quick Tips', navigateTo: '', accessibilityHint: ''},
     { name: 'How to Talk with Your Child', navigateTo: '', accessibilityHint: 'Navigates to...'},
-    { name: 'How to Deal with New Fears and Worries', navigateTo: '', accessibilityHint: 'Navigates to...'},
+    { name: 'How to Deal with New Fears and Worries', navigateTo: 'DealWorries', accessibilityHint: 'Navigates to...'},
     { name: "Working with Your Child's Healthcare Team", navigateTo: '', accessibilityHint: 'Navigates to...'},
     { name: 'Injury and Pain Care', navigateTo: '', accessibilityHint: 'Navigates to...'},
     { name: 'When to Get Outside Help', navigateTo: '', accessibilityHint: 'Navigates to...'},
