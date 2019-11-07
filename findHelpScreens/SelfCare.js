1c72a4379a1da3e0e046be54f090bca994cccbe4
Aaron <akhan727@gmail.com>
2019-11-06 23:57:30 -0700

    SelfCare/US14T174: I was able to successfully add the SelfCare image to my page. Although, I need to fix the border which I will do on T176.

--- findHelpScreens/SelfCare.js
+++ findHelpScreens/SelfCare.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-
+import React, { Component } from "react";
+import { View, StyleSheet, Text, ScrollView, Image } from "react-native";
+import { Video } from 'expo-av';
 
 class SelfCare extends Component {
   
@@ -12,10 +11,22 @@ class SelfCare extends Component {
   render() {
     return (
       <View style={styles.container}>
-                <Text>
-                    This is the Self Care Screen {'\n'}
-                </Text>
-        </View>
+          <Text style={{ backgroundColor: '#2089DC', color: 'white', fontWeight: 'bold', fontSize: 22, textAlign: 'center', alignSelf: 'stretch' }}>
+              Self Care
+          </Text>
+          <Image
+              source={require('../assets/SelfCare.jpg')}
+              style={{ height: 204, width: '100%', maxHeight: 254, borderColor: '#2089DC', borderWidth: 14, borderTopWidth: 0, borderBottomWidth: 0, marginBottom:-1}}
+              accessibilityLabel="Photo of worried mother on phone."
+              accessibilityHint="Photo of worried mother on phone."
+          />
+          <Text style={{ backgroundColor: '#2089DC', color: 'white', alignSelf: 'stretch', paddingLeft: 15, fontSize: 15, borderBottomWidth:2, borderBottomColor:'#2089DC'}}>
+              <Text style={{ fontWeight: 'bold' }}>
+                  Reactions you may notice in yourself. 
+              </Text> 
+              {"\n"}It is very important to take good care of yourself.
+          </Text>
+      </View>
     );
   }
 }
@@ -24,8 +35,17 @@ export default SelfCare;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+      flex: 1,
+      alignItems: 'center',
+      margin: 10,
+      backgroundColor: 'white',
+      shadowColor: "#000",
+      shadowOffset: {
+          width: 0,
+          height: 2,
+      },
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62,
+      elevation: 4,
     }
   });
