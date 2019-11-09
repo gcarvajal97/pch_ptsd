3fcec65c61e86b9cb809258c2e340b770b74d169
Anthony DeCosta <ardecost@asu.edu>
2019-11-09 14:12:58 -0800

    US17T148: DealWithWorries complete

--- findHelpScreens/DealWithWorries.js
+++ findHelpScreens/DealWithWorries.js
@@ -33,47 +33,60 @@ class DealWithWorries extends Component {
                     How Do New Fears Develop?
                     </Text>
                     <Text style={styles.paragraph}>
-                    When something scary happens, we often learn to treat things connected to it as if they were danger signals. The next time something reminds us of what happened, our body reacts as if we were in danger, even if we are safe.
-
-A trauma reminder can be anything – a place or a person, a sight or a sound, or even a smell. It might be something you hardly notice, till the scared feelings come flooding back.
-
-How do we get over new fears - dealing with emotional triggers?
-
-In managing emotional triggers, it can really help to face reminders over and over, in the course of normal life. We can feel very anxious at first, but each time we experience a trauma reminder and hang in there long enough to know that it does not lead to danger, the scared feelings tend to get weaker.
-
-It is natural for your child to want to stay away from things that remind and upset him. But too much avoidance can get in the way of the natural recovery process, and make it harder to get over feeling scared.
-
-As a parent, you play an important role in helping your child face new fears without getting stuck in avoidance.
-
-How can parents tell if fears or worries are really a problem?
-
-Ask yourself: Does feeling afraid or nervous stop your child from getting back to normal safe activities? Or stop him from enjoying things even if he manages to do them?
-
-If so, you can help your child deal with new fears and worries before they become a bigger problem.
-
-What can parents do?
-
-1. Help your child make changes that really do help keep him safe.
-
-Like wearing a helmet, crossing the street carefully, or telling you where he is going.
-2. Be a "reality check" for your child.
-
-If your child is nervous about doing something, talk together and help her figure out: "Is this really helping me stay safe?” OR “Am I avoiding it just so I will not feel nervous?"
-3. Help your child understand "trauma reminders"/emotional triggers and avoidance.
-
-Sometimes kids need help to figure out what makes them feel scared or nervous.
-4. Encourage your child.
-
-Help him keep doing those safe things that make him nervous -- and praise his courage when he does!
-5. Notice your own worries.
-
-Ask yourself: Do my worries stop me from letting my child do things he really should be allowed to do?
-Use a trusted friend or family member as a “reality check” for yourself – to help you sort through your worries about your child’s safety.
-When to get more help
-
-If you are not sure whether your child's worries or fears are realistic, or how to encourage your child to face things that he is avoiding, get some help.
-
-Talk with your family doctor, school counselor, or someone else you trust, or seek a counselor who can help you and your child in managing emotional triggers and with traumatic stress reactions.
+                    When something scary happens, we often learn to treat things connected to it as if they were danger signals. 
+                    The next time something reminds us of what happened, our body reacts as if we were in danger, even if we are safe.
+                    </Text>
+                    <Text style={styles.paragraph}>
+                    A trauma reminder can be anything: a place or a person, a sight or a sound, or even a smell. It might be something you hardly notice, 
+                    until the scared feelings come flooding back.
+                    </Text>
+                    <Text style={styles.headline}>
+                    How do we get over new fears? How do we deal with emotional triggers?
+                    </Text>
+                    <Text style={styles.paragraph}>
+                    In managing emotional triggers, it can really help to face reminders over and over, in the course of normal life. 
+                    We can feel very anxious at first, but each time we experience a trauma reminder and hang in there long enough to know that it does not lead to danger, 
+                    the scared feelings tend to get weaker.
+                    </Text>
+                    <Text style={styles.paragraph}>
+                    It is natural for your child to want to stay away from things that remind and upset him. 
+                    But too much avoidance can get in the way of the natural recovery process, and make it harder to get over feeling scared.
+                    </Text>
+                    <Text style={styles.paragraph}>
+                    As a parent, you play an important role in helping your child face new fears without getting stuck in avoidance.
+                    </Text>
+                    <Text style={styles.headline}>
+                    How can parents tell if fears or worries are really a problem?
+                    </Text>
+                    <Text style={styles.paragraph}>
+                    Ask yourself, does feeling afraid or nervous stop your child from getting back to normal safe activities? 
+                    Or does it stop him from enjoying things even if he manages to do them?
+                    </Text>
+                    <Text style={styles.paragraph}>If so, you can help your child deal with new fears and worries before they become a bigger problem.</Text>
+                    <Text style={styles.headline}>What can parents do?</Text>
+                    <Text style={styles.paragraphBold}>1. Help your child make changes that really do help keep him safe.</Text>
+                    <Text style={styles.bulletLight}>Things like wearing a helmet, crossing the street carefully, or telling you where he is going.</Text>
+                    <Text style={styles.paragraphBold}>2. Be a "reality check" for your child.</Text>
+                    <Text style={styles.bulletLight}>
+                        If your child is nervous about doing something, talk together and help them figure out: 
+                        "Is this really helping me stay safe?” OR “Am I avoiding it just so I will not feel nervous?"
+                    </Text>
+                    <Text style={styles.paragraphBold}>3. Help your child understand "trauma reminders," emotional triggers and avoidance.</Text>
+                    <Text style={styles.bulletLight}>Sometimes kids need help to figure out what makes them feel scared or nervous.</Text>
+                    <Text style={styles.paragraphBold}>4. Encourage your child.</Text>
+                    <Text style={styles.bulletLight}>Help them to keep doing those safe things that make them nervous. Praise their courage when they do!</Text>
+                    <Text style={styles.paragraphBold}>5. Notice your own worries.</Text>
+                    <Text style={styles.bulletLight}>Ask yourself: Do my worries stop me from letting my child do things he really should be allowed to do?</Text>
+                    <Text style={styles.bulletLight}>
+                        Use a trusted friend or family member as a “reality check” for yourself. This can help you sort through your worries about your child’s safety.
+                    </Text>
+                    <Text style={styles.headline}>When to get more help</Text>
+                    <Text style={styles.paragraph}>
+                        If you are not sure whether your child's worries or fears are realistic, or how to encourage your child to face things that they are avoiding, get some help.
+                    </Text>
+                    <Text style={styles.paragraph}>
+                        Talk with your family doctor, school counselor, or someone else you trust, 
+                        or seek a counselor who can help you and your child in managing emotional triggers and traumatic stress reactions.
                     </Text>
                 </ScrollView>
             </View>
