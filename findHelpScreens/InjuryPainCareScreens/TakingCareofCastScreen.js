844ad07de49271a859fd822e573e9b7006bdc7ff
leahtbriggs <leahtbriggs@gmail.com>
2019-11-13 09:10:50 -0800

    US34T169: Add in Taking Care of Your Child's Cast section
    Add in Taking Care of Your Child's Cast section and added initial text from website.

---
+++ findHelpScreens/InjuryPainCareScreens/TakingCareofCastScreen.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+  }
+});
+
+export default class TakingCareofCastScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          A cast holds a broken bone in place while it heals. Casts also help to prevent or decrease muscle contractions and can help limit movement, especially after surgery.
+
+          The outside of the cast is made from one of two types of casting materials:
+
+              Plaster – white in color
+              Fiberglass – comes in a variety of colors
+
+          Cotton and other synthetic materials are used to line the inside of the cast to make it soft and to provide padding around bony areas, such as the wrist or elbow.
+
+              Follow instructions provided by your healthcare provider (doctor or nurse practitioner) It is important that your child follow all of the special instructions given by your healthcare provider to make sure that the broken bone heals well. Some things that your child's healthcare provider might suggest:
+                  Keep the cast clean and dry. Do not get it wet in the shower or tub or swimming pool. Ask your doctor how you can buy a protective sleeve if you think your child might be somewhere where his cast might get wet. Check for cracks and breaks in the cast.
+                  Check for rough edges. These rough edges can be padded to protect the skin from scratches.
+                  Do not scratch the skin under the cast by inserting objects inside the cast – this can cut the skin.
+                  If your child complains of itching, use a hairdryer placed on a cool setting to blow air under the cast. Never blow warm or hot air into the cast.
+                  Do not put powders or lotions inside the cast.
+                  Cover the cast while your child is eating to prevent food spills and crumbs from entering the cast.
+                  Prevent small toys or objects from being put inside the cast.
+                  Elevate the cast above the level of the heart to decrease swelling.
+                  Encourage your child to move his or her fingers or toes to promote circulation.
+                  Older children with body casts may need to use a bedpan or urinal in order to go to the bathroom.
+                  Tips to keep body casts clean and dry and prevent skin irritation around the genital area include the following:
+                      Use a diaper or sanitary napkin around the genital area to prevent leakage or splashing urine.
+                      Place toilet paper inside the bedpan to prevent urine from splashing onto the cast or bed.
+                      Keep the genital area as clean and dry as possible to prevent skin irritation.
+                      
+              Bathing and Hair Washing
+                  Your child may need to have sponge baths until the cast is removed.
+                  How to wash your child's hair: Lay your child across a bed a place a bucket on the floor to catch the water as you shampoo. You can also get "no-rinse" shampoo at the drug store.
+                  
+              Make sure to ask your healthcare provider:
+                  How long will my child need to wear the cast?
+                  What are the limits to my child's activity while he wears the cast?
+                  What are the limits on my child's activity once you take off the cast?
+                  When can my child return to school?
+                  When can my child return to gym class? to contact sports? to biking? or to rough play?
+                  Will my child need physical therapy?
+                  Will this injury affect my child's growth?
+                  
+              When to call your healthcare provider. Ask your healthcare provider for specific signs and symptoms that should alert you to call them right away. They may include the following:
+                  Your child has increasing pain and pain medicine recommended or prescribed by your healthcare provider is not helping.
+                  You see a change in the cast; cracking, softening, drainage from the inside.
+                  You smell a bad odor coming from the cast.
+                  Your child's fingers or toes are cold, blue/gray or swollen.
+                  Your child feels numbness or tingling in the fingers or toes.
+                  Your child cannot move his/her fingers or toes.
+                  The cast is slipping off.
+                  There is increased swelling above or below the cast.
+                  The cast feels very tight.
+                  If you have any other concerns.
+                  
+              Full recovery goes beyond physical healing - learn more about your child's emotional recovery after an injury, and how you can help.
+        </Text>
+      </View>
+    );
+  }
+}
