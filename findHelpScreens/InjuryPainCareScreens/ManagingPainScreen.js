1c77855dc42f69390ea3b4795c57e8d1abc0588a
leahtbriggs <leahtbriggs@gmail.com>
2019-11-14 09:50:27 -0800

    US34T172: Add in Recognizing and Managing Pain Section
    Add in Recognizing and Managing Pain Section and text from website.

---
+++ findHelpScreens/InjuryPainCareScreens/ManagingPainScreen.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+  }
+});
+
+export default class ManagingPainScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+                <Text>
+                Nearly everyone has pain after an injury. For most children, our research has shown that the pain will get a lot better during the first week after getting care for an injury. If the pain gets worse, make sure to talk to your child's doctor.{'\n'}
+                Follow instructions provided by your healthcare provider It is important that your child follow all of the special instructions given by your healthcare provider and takes pain medicines as your healthcare provider told you to use them. The following are general suggestions about pain.{'\n'}
+
+                    Follow instructions provided by your healthcare provider It is important that your child follow all of the special instructions given by your healthcare provider and takes pain medicines as your healthcare provider told you to use them. The following are general suggestions about pain.
+                    1. 	How to recognize pain
+
+                    In your school-aged child:
+                    School-aged children may display different behaviors when they are in pain or uncomfortable. Your child may:
+
+                        Have difficulty sleeping
+                        Not eat or drink as much as usual
+                        Cry or moan
+                        Lose interest in play
+                        Become less active, lie quietly, or curl up in bed
+                        Be restless
+                        Complain of pain
+                        Hold or protect the area of discomfort
+                        Become "whiny" or "cranky"
+
+                    In your teen:
+                    Teens may display different behaviors when they are in pain or uncomfortable. Remember that teen athletes may not want to admit to pain if it keeps them out of sports. Your teen may:
+
+                        Become quiet
+                        Have difficulty sleeping
+                        Not eat or drink as much as usual
+                        Lose interest in family and friends
+                        Become less active, lie quietly, or curl up in bed
+                        Become irritable and angry
+                        Complain of pain
+
+                    2. 	How to help manage your child's pain
+
+                    Make sure to use the pain medicines in the doses and at the times that your healthcare professional recommended. Keep a pain diary, recording where your child's pain is, how bad it is, when it happens, what makes it better, and what makes it worse. There are many ways, other than medicine, to comfort your child. The healthcare team can make suggestions for your child, but here are some basics for getting started.
+
+                    For your school-aged child:
+                    A. 	Create a comfortable and safe environment
+
+                        Dim the lights
+                        Stay with your child as much as possible.
+                        Provide quiet time.
+                        Talk in a soothing voice.
+
+                    B. 	Use gentle and firm touch to provide comfort and security
+
+                        Hold your child.
+                        Massage your child's body in a firm, slow motion
+                        Hold your child's hand.
+                        Offer hugs.
+
+                    C. 	Use distraction to help your school-age child focus on something other than pain. Encourage your child to:
+
+                        Play their favorite music
+                        Blow bubbles
+                        Take slow, deep breaths
+                        Read a book
+                        Talk about a favorite place or memory
+                        Play a game or watch TV
+                        Do art projects, draw, or color
+
+                    For your teen:
+                    A. 	Create a comfortable and safe environment
+
+                        Dim the lights
+                        Offer to stay with your teen
+                        Reduce noise.
+                        Approach your teen in a calm, comforting way.
+
+                    B. 	Use gentle and firm touch to provide comfort and security
+
+                        Offer gentle massage.
+                        Offer hugs or hold your teen's hand
+                        Talk in a soothing voice.
+
+                    C. 	Use distraction to help your teen focus on something other than pain. Encourage your teen to:
+
+                        Play music
+                        Take slow, deep breaths
+                        Read a book
+                        Talk about a pleasant experience or memory.
+                        Play a game or watch TV
+                        Do art projects, draw, or color
+
+                    3. 	When to call your child's doctor Make sure to ask your healthcare provider what symptoms to look out for and whether you should call or go straight to the hospital. Some of these symptoms are:
+
+                        Your child has increasing pain and pain medicine recommended or prescribed by your healthcare provider is not helping.
+                        Your child has a new pain.
+                        If you have any other concerns
+
+                    4. 	Make sure to ask your healthcare provider:
+
+                        How do I keep a pain diary and what information should I record?
+                        What is the best way to assess my child's pain, given their age or level?.
+                        What symptoms should I look for in my child? How often should I check?
+                        What should I do if my child has a symptom you told me to watch for? Should I call the office, call 911, or go to the hospital?
+                        Can my child return to school?
+                        What are the limits to my child's activity?
+                        When can my child start gym? contact sports? biking? or rough play?
+                        Will you please call my child's school and explain how to manage my child's pain and anything else they need to know about my child?
+                        What kind of pain can be expected and for how long?
+                        How will the pain be controlled before, during and after any procedures?
+                        If your child receives pain medication, learn as much as you can about the medicine from the doctor, nurse, or pharmacist. Talk to your healthcare provider about any side effects your child might have and how to help treat them.
+                        How can I manage my child's pain during therapy if my child is going to get physical therapy?
+
+                    5. 	Suggestions for follow-up appointments when you bring your child in for his follow-up visits or with specialists, remember to:
+
+                        Let your healthcare provider know if your child is still having any pain.
+                        Let your doctor know where the pain is, what makes it better, what makes it worse.
+                        Bring all of your medicines with you to all appointments.
+                        If you are going to another specialist, try to bring along any medical records and copies of radiographs, MRIs, and CT scans.
+                        If your child is going to therapy, ask them to manage your child's pain.
+
+                    7. 	Full recovery goes beyond physical healing - learn more about your child's emotional recovery after an injury, and how you can help.
+                </Text>
+        </View>
+    );
+  }
+}
