d7927b17adee8d39d4fee95ea3f89d241ae986bf
Anthony DeCosta <ardecost@asu.edu>
2019-11-22 02:43:17 -0800

    US18T203: Shadow/Elevate added to View

--- QuizScreens/QuizMain.js
+++ QuizScreens/QuizMain.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, } from "react-native";
+import { View, StyleSheet, Text, Button } from "react-native";
 
 class QuizMain extends Component {
 
     // Gives the NavBar a title for this specific screen
     static navigationOptions = {
-        headerTitle: 'Quiz'
+        headerTitle: 'Learn More'
     }
-
     render() {
         return (
-            // renders the learn more screen
             <View style={styles.container}>
-                <Text>
-                    This is the Main Quiz Screen
+                <Text style={styles.paragraph}>
+                    Quiz Screen{'\n\n'}
+                    1. Some question? Radio buttons below{'\n\n'}
+                    2. Some other question. Radio buttons below{'\n\n'}
                 </Text>
-        </View>
+                <Button>Submit</Button>
+            </View>
         );
     }
 }
@@ -25,6 +26,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        margin: 10,
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+
+        elevation: 4,
+    },
+    bullet: {
+        marginVertical:1, 
+        marginHorizontal:18, 
+        textAlign:'left', 
+        alignSelf:'stretch', 
+        fontWeight:'bold',
+        fontSize:14,
+        paddingTop: 4,
+    },
+    paragraph: {
+        alignSelf:"center", 
+        paddingVertical:1, 
+        paddingHorizontal:5,
+        fontSize: 14,
+        marginTop: 5,
+    },
 });
