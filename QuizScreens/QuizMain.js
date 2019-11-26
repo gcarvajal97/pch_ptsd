3fb6f6d024c7fed3d620934d4da6929367bf108a
Anthony DeCosta <ardecost@asu.edu>
2019-11-25 22:31:06 -0800

    US18T201: Import content from website

--- QuizScreens/QuizMain.js
+++ QuizScreens/QuizMain.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { Button } from "react-native-elements";
 import NavigationService from "../components/NavigationService";
+
 
 class QuizMain extends Component {
 
@@ -11,12 +13,38 @@ class QuizMain extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.paragraph}>
-                    Quiz Screen{'\n\n'}
-                    1. Some question? Radio buttons below{'\n\n'}
-                    2. Some other question. Radio buttons below{'\n\n'}
-                </Text>
-                <Button onPress={()=>{NavigationService.navigate('QuizResults')}} title='Submit'></Button>
+                <ScrollView>
+                    <Text style={styles.paragraphTitle}>Rate Your Child's Reactions</Text>
+                    <Text style={styles.paragraph}>You can rate these on your own (based on what you have noticed with your child). If possible, it may
+                     even be better to involve your child in rating their own reactions with you.
+                    </Text>
+                    <Text style={styles.paragraph}>
+                        Re-experiencing: Reliving what happened{'\n\n'}
+                        1. Thinks a lot about what happened to him/her{'\n'}
+                        2. Has bad dreams or nightmares since the injury{'\n'}
+                        3. Gets upset or has physical reactions (headache, stomachache, heart pounding) at reminders of what happened{'\n\n'}
+                        Avoidance: Staying away from reminders{'\n\n'}
+                        4. Doesn’t want to talk about what happened or tries to push it out of his / her mind{'\n'}
+                        5. Wants to stay away from people, places, or things that are reminders of what happened{'\n'}
+                        6. Afraid of something that s/he was not afraid of before (or a previous fear or worry seems to get worse){'\n'}
+                        7. Not interested in usual activities, since the injury{'\n'}
+                        8. Not interested in being with people s/he usually likes, since the injury{'\n\n'}
+                        Hyper-arousal: Feeling anxious or jumpy{'\n\n'}
+                        9. Worries a lot that something else bad will happen{'\n'}
+                        10. Startles easily – for example, jumps if there is a sudden noise{'\n'}
+                        11. Seems irritable or has angry outbursts, since the injury{'\n'}
+                        12. Has trouble paying attention to things, since the injury{'\n'}
+                        13. Has trouble falling or staying asleep, since the injury{'\n\n'}
+                        Other concerns about your child{'\n\n'}
+                        14. Is still having pain or discomfort.{'\n'}
+                        15. Has had other behavior changes since the injury{'\n'}
+                        16. Is having trouble with returning to school or other activities{'\n\n'}
+                        Concerns about how others in the family are coping{'\n\n'}
+                        17. Brothers or sisters are upset or worried.{'\n'}
+                        18. You (parent) are stressed or worried yourself{'\n'}
+                    </Text>
+                    <Button onPress={() => { NavigationService.navigate('QuizResults') }} title='Submit' raised containerStyle={{marginHorizontal:120, marginBottom:10}}></Button>
+                </ScrollView>
             </View>
         );
     }
@@ -40,19 +68,30 @@ const styles = StyleSheet.create({
         elevation: 4,
     },
     bullet: {
-        marginVertical:1, 
-        marginHorizontal:18, 
-        textAlign:'left', 
-        alignSelf:'stretch', 
-        fontWeight:'bold',
-        fontSize:14,
+        marginVertical: 1,
+        marginHorizontal: 18,
+        textAlign: 'left',
+        alignSelf: 'stretch',
+        fontWeight: 'bold',
+        fontSize: 14,
         paddingTop: 4,
     },
     paragraph: {
-        alignSelf:"center", 
-        paddingVertical:1, 
-        paddingHorizontal:5,
+        alignSelf: "center",
+        paddingVertical: 1,
+        paddingHorizontal: 5,
         fontSize: 14,
         marginTop: 5,
     },
+    paragraphTitle: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        alignSelf: 'stretch',
+        textAlign: 'left',
+        marginTop: 3,
+        marginBottom: 2,
+        color: '#2089DC',
+        paddingLeft: 4,
+        paddingRight: 1
+    }
 });
