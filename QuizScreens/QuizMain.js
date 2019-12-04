e3bbb8d8f9774d9d9ee078c223967a997922244e
leahtbriggs <leahtbriggs@gmail.com>
2019-12-03 16:54:46 -0800

    US18T216: Interpret information from Quiz Page
    Interpret information from the quiz page and display the relevent information.

--- QuizScreens/QuizMain.js
+++ QuizScreens/QuizMain.js
@@ -9,10 +9,10 @@ class QuizMain extends Component {
         super()
         // State is set for every question with an initial state, submit button will pass the current state of each question when pressed.
         this.state = {
-          question01: 2, question02: 2, question03: 2, question04: 2, question05: 2,
-          question06: 2, question07: 2, question08: 2, question09: 2, question10: 2,
-          question11: 2, question12: 2, question13: 2, question14: 2, question15: 2, 
-          question16: 2, question17: 2, question18: 2
+          1: 2, 2: 2, 3: 2, 4: 2, 5: 2,
+          6: 2, 7: 2, 8: 2, 9: 2, 10: 2,
+          11: 2, 12: 2, 13: 2, 14: 2, 15: 2, 
+          16: 2, 17: 2, 18: 2
         }
       }
 
@@ -31,63 +31,63 @@ class QuizMain extends Component {
                      even be better to involve your child in rating their own reactions with you.</Text>
                     <Text style={styles.paragraph}>
                         Re-experiencing: Reliving what happened{'\n\n'}
-                        1. Thinks a lot about what happened to him/her</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question01': e})} selectedIndex={this.state.question01} containerStyle={{height:30}}/>
+                        1. Thinks a lot about what happened to them</Text>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'1': e})} selectedIndex={this.state[1]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         2. Has bad dreams or nightmares since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question02': e})} selectedIndex={this.state.question02} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'2': e})} selectedIndex={this.state[2]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         3. Gets upset or has physical reactions (headache, stomachache, heart pounding) at reminders of what happened</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question03': e})} selectedIndex={this.state.question03} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'3': e})} selectedIndex={this.state[3]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         Avoidance: Staying away from reminders{'\n\n'}
-                        4. Doesn’t want to talk about what happened or tries to push it out of his / her mind</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question04': e})} selectedIndex={this.state.question04} containerStyle={{height:30}}/>
+                        4. Doesn’t want to talk about what happened or tries to push it out of their mind</Text>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'4': e})} selectedIndex={this.state[4]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         5. Wants to stay away from people, places, or things that are reminders of what happened</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question05': e})} selectedIndex={this.state.question05} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'5': e})} selectedIndex={this.state[5]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         6. Afraid of something that they were not afraid of before (or a previous fear or worry seems to get worse)</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question06': e})} selectedIndex={this.state.question06} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'6': e})} selectedIndex={this.state[6]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         7. Not interested in usual activities, since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question07': e})} selectedIndex={this.state.question07} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'7': e})} selectedIndex={this.state[7]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         8. Not interested in being with people they usually like being with, since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question08': e})} selectedIndex={this.state.question08} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'8': e})} selectedIndex={this.state[8]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         Hyper-arousal: Feeling anxious or jumpy{'\n\n'}
                         9. Worries a lot that something else bad will happen</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question09': e})} selectedIndex={this.state.question09} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'9': e})} selectedIndex={this.state[9]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         10. Startles easily – for example, jumps if there is a sudden noise</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question10': e})} selectedIndex={this.state.question10} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'10': e})} selectedIndex={this.state[10]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         11. Seems irritable or has angry outbursts, since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question11': e})} selectedIndex={this.state.question11} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'11': e})} selectedIndex={this.state[11]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         12. Has trouble paying attention to things, since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question12': e})} selectedIndex={this.state.question12} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'12': e})} selectedIndex={this.state[12]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         13. Has trouble falling or staying asleep, since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question13': e})} selectedIndex={this.state.question13} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'13': e})} selectedIndex={this.state[13]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         Other concerns about your child{'\n\n'}
-                        14. Is still having pain or discomfort.</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question14': e})} selectedIndex={this.state.question14} containerStyle={{height:30}}/>
+                        14. Is still having pain or discomfort</Text>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'14': e})} selectedIndex={this.state[14]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         15. Has had other behavior changes since the injury</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question15': e})} selectedIndex={this.state.question15} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'15': e})} selectedIndex={this.state[15]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         16. Is having trouble with returning to school or other activities</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question16': e})} selectedIndex={this.state.question16} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'16': e})} selectedIndex={this.state[16]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         Concerns about how others in the family are coping{'\n\n'}
-                        17. Brothers or sisters are upset or worried.</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question17': e})} selectedIndex={this.state.question17} containerStyle={{height:30}}/>
+                        17. Brothers or sisters are upset or worried</Text>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'17': e})} selectedIndex={this.state[17]} containerStyle={{height:30}}/>
                         <Text style={styles.paragraph}>
                         18. You (parent) are stressed or worried yourself</Text>
-                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'question18': e})} selectedIndex={this.state.question18} containerStyle={{height:30}}/>
+                        <ButtonGroup buttons={buttons} onPress={(e) => this.setState({'18': e})} selectedIndex={this.state[18]} containerStyle={{height:30}}/>
                         <Text style={styles.finePrint}>Note: This is not a formal medical or psychological assessment or diagnosis.{'\n'}
                                 If you are concerned with how you or your child is doing, please contact your doctor, 
                                 mental health professional, or other healthcare provider.{'\n\n'}
--- QuizScreens/QuizResults.js
+++ QuizScreens/QuizResults.js
@@ -1,5 +1,28 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, } from "react-native";
+import {Card, Button as CardButton} from 'react-native-elements';
+import { ScrollView } from "react-native-gesture-handler";
+
+const content = {
+    1: {title: 'Thinks a lot about what happened to them', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '},
+    2: {title: 'Has bad dreams or nightmares since the injury', body: ' Suspendisse nec placerat risus. Nunc congue dolor at mauris tristique luctus. Duis posuere quam gravida tincidunt laoreet. Phasellus quis nulla vitae lacus egestas maximus.'},
+    3: {title: 'Gets upset or has physical reactions at reminders of what happened', body: 'Suspendisse at nunc orci. Aenean est turpis, finibus ut commodo vel, suscipit quis augue. Sed convallis augue ac nisl semper facilisis.'},
+    4: {title: 'Doesn’t want to talk about what happened or tries to push it out of their mind', body: 'Cras libero nisi, lacinia rutrum urna non, tincidunt elementum nibh. Sed finibus volutpat metus, at viverra dui dictum non. Fusce dictum est at purus tempus auctor. Nulla odio ante, molestie non neque sed, eleifend consectetur tortor. Aenean ut rutrum libero. '},
+    5: {title: 'Wants to stay away from people, places, or things that are reminders of what happened', body: 'Vestibulum quis arcu in purus congue pellentesque. Sed in nulla molestie, volutpat leo vel, efficitur ante.'},
+    6: {title: 'Afraid of something that they were not afraid of before (or a previous fear or worry seems to get worse)', body: 'Cras mi sem, viverra non scelerisque eget, ultricies sed mi. Donec ac ornare lacus, sed semper arcu. Quisque vel neque in tellus volutpat accumsan sed eget lacus.'},
+    7: {title: 'Not interested in usual activities, since the injury', body: 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent tincidunt nibh a euismod interdum. Maecenas gravida nulla ut tellus porta placerat. '},
+    8: {title: 'Not interested in being with people they usually like being with, since the injury', body: 'Cras iaculis auctor mi vitae lobortis. Donec sit amet dignissim sem. Donec hendrerit iaculis nulla, vitae ornare magna euismod pharetra. '},
+    9: {title: 'Worries a lot that something else bad will happen', body: 'Morbi pulvinar venenatis sapien vel condimentum. Phasellus nibh nulla, interdum et suscipit et, tincidunt quis tortor. In nec mattis nisl. Maecenas eros sapien, scelerisque et nunc sollicitudin, pretium lacinia lacus.'},
+    10: {title: 'Startles easily – for example, jumps if there is a sudden noise', body: 'Duis iaculis vulputate mi, a accumsan felis imperdiet ac. Nullam ut vehicula est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. '},
+    11: {title: 'Seems irritable or has angry outbursts, since the injury', body: 'Praesent suscipit lacus vel ullamcorper egestas. Vestibulum sodales enim mi, id ultricies velit aliquam vitae. In dapibus dictum urna, id posuere dui dignissim id. Nulla imperdiet justo orci, et tincidunt tellus aliquet vitae.'},
+    12: {title: 'Has trouble paying attention to things, since the injury', body: 'Nam lacus magna, dignissim eleifend enim id, varius molestie est. Nulla finibus nunc at risus blandit, luctus porttitor libero sodales. In sit amet eros id neque venenatis tristique. '},
+    13: {title: 'Has trouble falling or staying asleep, since the injury', body: 'Nam ipsum nulla, porta et erat eu, aliquam vestibulum ante. Nam sit amet posuere odio, eu finibus nunc. Etiam vitae enim mauris. Pellentesque tincidunt elit non tortor tincidunt mollis eu non est. '},
+    14: {title: 'Is still having pain or discomfort', body: 'Cras laoreet id augue id dignissim. Etiam ullamcorper luctus mauris eget mollis. Aenean vel turpis non magna laoreet volutpat. '},
+    15: {title: 'Has had other behavior changes since the injury', body: 'Duis cursus orci quis eros ultricies, sit amet vestibulum justo congue. Mauris porta est non porta eleifend.'},
+    16: {title: 'Is having trouble with returning to school or other activities', body: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque ut ante viverra, mollis odio quis, hendrerit elit. Quisque dignissim nisi quis lectus placerat, eget viverra arcu fringilla. '},
+    17: {title: 'Brothers or sisters are upset or worried', body: 'Vestibulum quis semper orci, at porta magna. Quisque et urna congue, ultrices augue nec, vestibulum mi. Integer ante est, interdum sit amet felis eget, blandit rutrum risus.'},
+    18: {title: 'You (parent) are stressed or worried yourself', body: 'Sed rutrum efficitur turpis, eu rhoncus nisl molestie sit amet. In vitae enim facilisis, varius nisl ac, condimentum magna.'},
+};
 
 export default class QuizResults extends Component {
 
@@ -12,17 +35,20 @@ export default class QuizResults extends Component {
         // Receive results using props, apparently only way to do this. RN has an issue with params and StackNavigator at the moment.
         // It still works as intended, but I was trying to use the modern practice of NavigationActions and creating a NavigationService. 
         const results = this.props.navigation.getParam('results', 'nothing found');
-        
+
         return (
             <View style={styles.container}>
-                <Text>
-                    This is the Quiz Results Screen
-                </Text>
-                {
-                // Just an example of iterating through the results using map and rendering to the page.
-                Object.entries(results).map(([key,v])=>{
-                    return <Text key={key}>{key +": value = "+ v}</Text>})
+                <ScrollView>
+                {                
+                    Object.entries(results).map(([key,v])=>{
+                        if (v <= 1) {
+                            return <Card featuredTitle={content[key].title} image={require('../assets/FindWaysHelp.jpg')} >
+                            <Text style={{ marginBottom: 5 }}> {content[key].body} </Text>
+                            </Card>
+                        }
+                    })
                 }
+                </ScrollView>
         </View>
         );
     }
@@ -60,5 +86,13 @@ const styles = StyleSheet.create({
         paddingHorizontal:5,
         fontSize: 14,
         marginTop: 5,
+    },
+    title: {
+        backgroundColor: '#2089DC', 
+        color: 'white', 
+        fontWeight: 'bold', 
+        fontSize: 22, 
+        textAlign: 'center', 
+        alignSelf: 'stretch'
     },
 });
