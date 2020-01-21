6108e3c689e2855a3deecabcce1513bfe91491c3
Carvajal, Gianni <gcarvaj3@asu.edu>
2020-01-21 09:35:43 -0600

    US223T253: Get Localization working and example going

--- mainAndLists/LearnMoreList.js
+++ mainAndLists/LearnMoreList.js
@@ -7,30 +7,53 @@ import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 
 // List of screens that will be dispalyed in this list component
-const listNames = [
+const listNamesEn = [
     { name: 'You Are Not Alone', navigateTo: 'NotAlone', accessibilityHint: 'Navigates to the you are not alone screen'},
     { name: 'Reactions to Injury', navigateTo: 'Reactions', accessibilityHint: 'Navigates to the reactions to injury screen'},
     { name: 'What Are Traumatic Stress Reactions?', navigateTo: 'TraumaticStressReactions', accessibilityHint: 'Navigates to the what are traumatic stress reactions screen'},
     { name: 'How Long Do Traumatic Stress Reactions Last?', navigateTo: 'HowLong', accessibilityHint: 'Navigates to the how long do traumatic stress reactions last screen'},
 ]
 
+const listNamesEs = [
+    { name: 'You Are Not Alone', navigateTo: 'NotAlone', accessibilityHint: 'Navigates to the you are not alone screen'},
+    { name: 'Reactions to Injury', navigateTo: 'Reactions', accessibilityHint: 'Navigates to the reactions to injury screen'},
+    { name: 'What Are Traumatic Stress Reactions?', navigateTo: 'TraumaticStressReactions', accessibilityHint: 'Navigates to the what are traumatic stress reactions screen'},
+    { name: 'How Long Do Traumatic Stress Reactions Last?', navigateTo: 'HowLong', accessibilityHint: 'Navigates to the how long do traumatic stress reactions last screen'},
+]
+
+i18n.translations = {
+    en: {
+        header: 'Learn More',
+        title: 'WHAT TO EXPECT AFTER INJURY',
+        listNames: listNamesEn
+    },
+    es: {
+        header: 'Aprende más',
+        title: 'QUE ESPERAR DESPUÉS DE LESIÓN',
+        listNames: listNamesEs
+    }
+}
+
+i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+
 export default class LearnMoreList extends Component {
 
     // Gives the NavBar a title for this specific screen
     static navigationOptions = {
-        headerTitle: 'Learn More'
+        headerTitle: i18n.t('header')
     }
     render() {
         return (
             <View>
                 <Card containerStyle={{margin:5}}>
                     <Text style={{textAlign: 'center', backgroundColor:'#2089DC', fontSize:20, color:'white', padding:10, paddingVertical:10, margin:0}}>
-                        WHAT TO EXPECT AFTER INJURY
+                        {i18n.t('title')}
                     </Text>
                     <Divider style={{backgroundColor:'black', height:1, marginBottom:-1}}/>
                     <View>
                         {
-                            listNames.map((l, i) => {
+                            i18n.t('listNames').map((l, i) => {
                                 return (
                                     <ListItem
                                         containerStyle={{paddingLeft:14, paddingVertical:12, paddingRight:0}}
