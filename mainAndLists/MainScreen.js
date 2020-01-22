75906663d1ccdc8674effb87d9e4de754f6f4461
Justin Tullos <justin.tullos@jbhunt.com>
2020-01-21 23:15:33 -0600

    US225T248
    WIP

--- mainAndLists/MainScreen.js
+++ mainAndLists/MainScreen.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { View, Button, ScrollView } from "react-native";
-
-import LearnMoreCard from '../components/LearnMoreCard';
-import QuizCard from '../components/QuizCard';
+import { Button, ScrollView, View } from "react-native";
 import FindHelpCard from '../components/FindHelpCard';
-
+import LearnMoreCard from '../components/LearnMoreCard';
 import NavigationService from '../components/NavigationService';
+import QuizCard from '../components/QuizCard';
+import * as Localization from 'expo-localization';
+
+// Stuck this here for now, since this is the entrypoint of our application
+export const LOCALE = Localization.locale;
+
     // I have learned that Props will become deprecated soon, this code has been changed to follow the
     // new best practice in React (to hopefully avoid features breaking in future updates).
     // NavigationService is a new file I created, allowing me to use the openDrawer function.
@@ -13,7 +16,7 @@ class MainScreen extends Component {
 
     static navigationOptions = () => ({
         headerTitle: 'Pediatric PTSD',
-        headerLeft: (<View style={{ margin: 5 }}><Button 
+        headerLeft: (<View style={{ margin: 5 }}><Button
             onPress={()=>NavigationService.openDrawer()}
             title='More'></Button></View>)
     })
