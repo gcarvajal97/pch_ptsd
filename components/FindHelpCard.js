ead736aa6ac5380c8578044325a4d28c828bc849
Anthony DeCosta <ardecost@asu.edu>
2020-01-22 02:52:10 -0800

    T236: Translate FindMore on MainScreen

--- components/FindHelpCard.js
+++ components/FindHelpCard.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
-import {Card, Button as CardButton} from 'react-native-elements';
-// react-native and react-native-elements each have their Button components.
-// To ensure we are using the correct one in the Card, I have imported Button as CardButton from elements
-
+import { Text } from 'react-native';
+import { Card, Button as CardButton } from 'react-native-elements';
 import NavigationService from './NavigationService';
+import translate from './translateService';
+
 // Card view component to be displayed on the main screen
 // We can now use the NavigationService to navigate to screens as seen below (NavigationService.navigate('WhateverScreen'))
 export default class FindHelpCard extends Component {
     render() {
         return(
         <Card
-            featuredTitle="Help Your Child Recover"
+            featuredTitle={translate('mainScreen.findHelpTitle')}
             image={require('../assets/FindWaysHelp.jpg')}
             accessible
-            accessibilityLabel="Photo of father reading a book to a baby on his lap.">
+            accessibilityLabel={translate('mainScreen.findHelpAccessLabel')}>
             <Text style={{ marginBottom: 5 }}>
-                Dealing with emotional reactions and injury care can be difficult. Find ways to help your child recover. 
+                {translate('mainScreen.findHelpCaption')} 
             </Text>
             <CardButton onPress={()=>NavigationService.navigate('FindHelp')}
                 buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                title='FIND HELP' 
+                title={translate('mainScreen.findHelpButton')} 
                 accessible
-                accessibilityLabel="Find Help"
-                accessibilityHint="Navigates to the find help screen"
+                accessibilityLabel={translate('mainScreen.findHelpButton')}
+                accessibilityHint={translate('mainScreen.findHelpAccessHint')}
                 />
         </Card>
         );
