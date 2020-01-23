2e2e0c2bca3efb108bcb808b02e89ae8d8c4a5f1
Justin Tullos <justin.tullos@jbhunt.com>
2020-01-22 20:32:09 -0600

    US225T248 Fix some formatting

--- components/shared/PchPtsdAccordion.js
+++ components/shared/PchPtsdAccordion.js
@@ -4,91 +4,95 @@ import * as Animatable from "react-native-animatable";
 import Accordion from "react-native-collapsible/Accordion";
 
 class PchPtsdAccordion extends Component {
-  state = {
-    sections: [],
-    activeSections: []
-  };
+    state = {
+        sections: [],
+        activeSections: []
+    };
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state.sections = this._initializeSections(
-      props.sections,
-      props.contentRenderFunction
-    );
-  }
+        this.state.sections = this._initializeSections(
+            props.sections,
+            props.contentRenderFunction
+        );
+    }
 
-  _initializeSections(sections, contentRenderFunction) {
-    return sections.map(section => {
-      return {
-        name: section.name,
-        accessibilityHint: section.accessibilityHint,
-        content: contentRenderFunction(section.content)
-      };
-    });
-  }
+    _initializeSections(sections, contentRenderFunction) {
+        return sections.map(section => {
+            return {
+                name: section.name,
+                accessibilityHint: section.accessibilityHint,
+                content: contentRenderFunction(section.content)
+            };
+        });
+    }
 
-  _renderHeader = (section, i, active) => {
-    return (
-      <Animatable.View
-        duration={400}
-        transition="backgroundColor"
-        style={(styles.header, active ? styles.active : styles.inactive)}
-      >
-        <View style={styles.headerTextContainer}>
-          <Text style={styles.headerText}>{active ? "[-]" : "[+]"}</Text>
-          <Text style={styles.headerText}>{section.name}</Text>
-        </View>
-      </Animatable.View>
-    );
-  };
+    _renderHeader = (section, i, active) => {
+        return (
+            <Animatable.View
+                duration={400}
+                transition="backgroundColor"
+                style={
+                    (styles.header, active ? styles.active : styles.inactive)
+                }
+            >
+                <View style={styles.headerTextContainer}>
+                    <Text style={styles.headerText}>
+                        {active ? "[-]" : "[+]"}
+                    </Text>
+                    <Text style={styles.headerText}>{section.name}</Text>
+                </View>
+            </Animatable.View>
+        );
+    };
 
-  _renderContent = (section, i, active) => {
-    return <Animatable.View>{section.content}</Animatable.View>;
-  };
+    _renderContent = (section, i, active) => {
+        return <Animatable.View>{section.content}</Animatable.View>;
+    };
 
-  _updateSections = activeSections => {
-    this.setState({ activeSections });
-  };
+    _updateSections = activeSections => {
+        this.setState({ activeSections });
+    };
 
-  render() {
-    return (
-      <View>
-        <Accordion
-          duration={500}
-          touchableComponent={TouchableOpacity}
-          sections={this.state.sections}
-          activeSections={this.state.activeSections}
-          renderHeader={this._renderHeader}
-          renderContent={this._renderContent}
-          onChange={this._updateSections}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Accordion
+                    duration={500}
+                    touchableComponent={TouchableOpacity}
+                    sections={this.state.sections}
+                    activeSections={this.state.activeSections}
+                    renderHeader={this._renderHeader}
+                    renderContent={this._renderContent}
+                    onChange={this._updateSections}
+                />
+            </View>
+        );
+    }
 }
 
 export default PchPtsdAccordion;
 
 const styles = StyleSheet.create({
-  header: {
-    backgroundColor: "#F5FCFF",
-    padding: 10
-  },
-  headerTextContainer: {
-    flex: 1,
-    flexDirection: "row",
-    margin: 10
-  },
-  headerText: {
-    paddingLeft: 5,
-    fontSize: 14,
-    fontWeight: "500"
-  },
-  active: {
-    backgroundColor: "rgba(255,255,255,1)"
-  },
-  inactive: {
-    backgroundColor: "rgba(245,252,255,1)"
-  }
+    header: {
+        backgroundColor: "#F5FCFF",
+        padding: 10
+    },
+    headerTextContainer: {
+        flex: 1,
+        flexDirection: "row",
+        margin: 10
+    },
+    headerText: {
+        paddingLeft: 5,
+        fontSize: 14,
+        fontWeight: "500"
+    },
+    active: {
+        backgroundColor: "rgba(255,255,255,1)"
+    },
+    inactive: {
+        backgroundColor: "rgba(245,252,255,1)"
+    }
 });
--- drawerScreens/glossaryScreens/GlossaryScreen.js
+++ drawerScreens/glossaryScreens/GlossaryScreen.js
@@ -8,47 +8,47 @@ import GlossaryTerms from "./GlossaryTerms";
 import translate from "../../components/translateService";
 
 class GlossaryScreen extends Component {
-  static navigationOptions = {
-    headerTitle: translate("glossary.glossary"),
-    headerLeft: (
-      <View style={{ margin: 5 }}>
-        <Button
-          onPress={() => NavigationService.navigateDrawer("Home")}
-          title={translate("glossary.title")}
-        ></Button>
-      </View>
-    )
-  };
+    static navigationOptions = {
+        headerTitle: translate("glossary.glossary"),
+        headerLeft: (
+            <View style={{ margin: 5 }}>
+                <Button
+                    onPress={() => NavigationService.navigateDrawer("Home")}
+                    title={translate("glossary.title")}
+                ></Button>
+            </View>
+        )
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-          <GlossaryScreenCard />
-          <PchPtsdAccordion
-            sections={translate("glossary.accordion")}
-            contentRenderFunction={GlossaryTerms}
-          />
-        </ScrollView>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+                    <GlossaryScreenCard />
+                    <PchPtsdAccordion
+                        sections={translate("glossary.accordion")}
+                        contentRenderFunction={GlossaryTerms}
+                    />
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 export default GlossaryScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: "white",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "white",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4
+    }
 });
