12b129ccc848be13f0d102023f70440be7be4b23
Justin Tullos <tullosjr@gmail.com>
2020-01-25 15:54:59 -0600

    Glossary/us225 (#36)
    * US225T248
    
    Add glossary.json, GlossaryTerm component
    
    Going to do a little refactoring, which should eliminate
    a substantial amount of code in this component.
    
    WIP, while I get my feet under me for
    
    * US225T248
    
    Revise glossary model
    
    * US225T248
    
    Revise one more property
    
    * US225T248
    
    Refactor glossary accordion, still WIP, but the core functionality
    is there.
    
    * US225T248
    
    Fix styling and some issues with the accordion in the glossary.
    
    * US225T248
    
    Refactor shared accordion component, finish English transcription
    into glossary.json, delete unnecessary components.
    
    * US225T248
    
    WIP
    
    * US225T248
    
    Fix i18n initialization things
    
    * US225T248
    
    Add rest of the accordion content, finalize glossary translation
    
    * US225T248
    
    Add glossary.json, GlossaryTerm component
    
    Going to do a little refactoring, which should eliminate
    a substantial amount of code in this component.
    
    WIP, while I get my feet under me for
    
    * US225T248
    
    Revise glossary model
    
    * US225T248
    
    Revise one more property
    
    * US225T248
    
    Refactor glossary accordion, still WIP, but the core functionality
    is there.
    
    * US225T248
    
    Fix styling and some issues with the accordion in the glossary.
    
    * US225T248
    
    Refactor shared accordion component, finish English transcription
    into glossary.json, delete unnecessary components.
    
    * US225T248
    
    WIP
    
    * US225T248
    
    Fix i18n initialization things
    
    * US225T248
    
    Add rest of the accordion content, finalize glossary translation
    
    * US225T248
    
    Add glossary.json, GlossaryTerm component
    
    Going to do a little refactoring, which should eliminate
    a substantial amount of code in this component.
    
    WIP, while I get my feet under me for
    
    * US225T248
    
    Revise glossary model
    
    * US225T248
    
    Revise one more property
    
    * US225T248
    
    Refactor glossary accordion, still WIP, but the core functionality
    is there.
    
    * US225T248
    
    Fix styling and some issues with the accordion in the glossary.
    
    * US225T248
    
    Refactor shared accordion component, finish English transcription
    into glossary.json, delete unnecessary components.
    
    * US225T248
    
    WIP
    
    * US225T248
    
    Fix i18n initialization things
    
    * US225T248
    
    Add rest of the accordion content, finalize glossary translation
    
    * US225T248
    
    Add glossary.json, GlossaryTerm component
    
    Going to do a little refactoring, which should eliminate
    a substantial amount of code in this component.
    
    WIP, while I get my feet under me for
    
    * US225T248
    
    Refactor glossary accordion, still WIP, but the core functionality
    is there.
    
    * US225T248
    
    Fix styling and some issues with the accordion in the glossary.
    
    * US225T248
    
    Refactor shared accordion component, finish English transcription
    into glossary.json, delete unnecessary components.
    
    * US225T248
    
    WIP
    
    * US225T248
    
    Fix i18n initialization things
    
    * US225T248
    
    Add rest of the accordion content, finalize glossary translation
    
    * US225T248
    
    Remove console.log
    
    * US225T248 Fix merge conflicts
    
    * US225T248 Fix some formatting
    
    * US225T248 Correct expo version, fix styling, fix translations

---
+++ components/shared/PchPtsdAccordion.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as Animatable from "react-native-animatable";
+import Accordion from "react-native-collapsible/Accordion";
+
+class PchPtsdAccordion extends Component {
+    state = {
+        sections: [],
+        activeSections: []
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state.sections = this._initializeSections(
+            props.sections,
+            props.contentRenderFunction
+        );
+    }
+
+    _initializeSections(sections, contentRenderFunction) {
+        return sections.map(section => {
+            return {
+                name: section.name,
+                accessibilityHint: section.accessibilityHint,
+                content: contentRenderFunction(section.content)
+            };
+        });
+    }
+
+    _renderHeader = (section, i, active) => {
+        return (
+            <Animatable.View
+                duration={400}
+                transition="backgroundColor"
+                style={
+                    (styles.header, active ? styles.active : styles.inactive)
+                }
+            >
+                <View style={styles.headerTextContainer}>
+                    <Text style={styles.headerText}>
+                        {active ? "[-]" : "[+]"}
+                    </Text>
+                    <Text style={styles.headerText}>{section.name}</Text>
+                </View>
+            </Animatable.View>
+        );
+    };
+
+    _renderContent = (section, i, active) => {
+        return <Animatable.View>{section.content}</Animatable.View>;
+    };
+
+    _updateSections = activeSections => {
+        this.setState({ activeSections });
+    };
+
+    render() {
+        return (
+            <View>
+                <Accordion
+                    duration={500}
+                    touchableComponent={TouchableOpacity}
+                    sections={this.state.sections}
+                    activeSections={this.state.activeSections}
+                    renderHeader={this._renderHeader}
+                    renderContent={this._renderContent}
+                    onChange={this._updateSections}
+                />
+            </View>
+        );
+    }
+}
+
+export default PchPtsdAccordion;
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: "#F5FCFF",
+        padding: 10
+    },
+    headerTextContainer: {
+        flex: 1,
+        flexDirection: "row",
+        margin: 10
+    },
+    headerText: {
+        paddingLeft: 5,
+        fontSize: 14,
+        fontWeight: "500"
+    },
+    active: {
+        backgroundColor: "rgba(255,255,255,1)"
+    },
+    inactive: {
+        backgroundColor: "rgba(245,252,255,1)"
+    }
+});
