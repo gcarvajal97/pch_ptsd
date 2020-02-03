437019641b3b6ca648c90d7ace2d4e1115e9eb0c
leahtbriggs <leahtbriggs@gmail.com>
2020-02-02 17:27:37 -0800

    US224T246: Add Spanish translations to Outside Help Pages
    Added Spanish translations to Outside Help Pages and that pages collapsible sub-pages.

--- findHelpScreens/outsideHelpScreens/HowToLook.js
+++ findHelpScreens/outsideHelpScreens/HowToLook.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text} from 'react-native';
 import { Linking } from "expo";
+import { translate } from '../../components/translateService';
 
 export default class HowToLook extends Component {
 
   render() {
     return (
       <View>
-        <Text style={styles.paragraph}>
-        There are very good and helpful treatments for children who continue to have traumatic stress symptoms.
-        </Text>
-        <Text style={styles.paragraph}>
-        If you are wondering how to find a child therapist, a good place to start is to talk 
-        with your child's doctor or a counselor at your child's school. 
-        They can help you figure out the best way of helping your child and your family, 
-        and may be able to suggest a counselor or therapist experienced in helping 
-        children with traumatic stress.
-        </Text>
-        <Text style={styles.paragraph}>
-        When looking for a counselor or therapist to help your child 
-        with traumatic stress symptoms, it's a good idea to ask 
-        questions such as: Do you have experience working with 
-        children after trauma? What is your approach to working 
-        with this type of problem? What is your success rate? 
-        How do you work with parents?
-        </Text>
+        <Text style={styles.paragraph}>{translate('howToLook.paragraph1')}</Text>
+        <Text style={styles.paragraph}>{translate('howToLook.paragraph2')}</Text>
+        <Text style={styles.paragraph}>{translate('howToLook.paragraph3')}</Text>
         <Text style={styles.paragraph}>
         {"\ "}
         <Text style={styles.link} onPress={()=> Linking.openURL('http://www.nctsnet.org/nccts/nav.do?pid=ctr_aud_prnt_gethelp')}
           accessible
-          accessibilityLabel="Click here for mental health professional information"
-          accessibilityHint="Navigates to a website on mental health professional information">
-          The National Child Traumatic Stress Network
+          accessibilityLabel={translate('howToLook.accessibilityLabel')}
+          accessibilityHint={translate('howToLook.accessibilityHint')}>
+          {translate('howToLook.linkName')}
           </Text>
-        {"\ "}has useful tips and information on how to find a mental health 
-        professional to help your child and family after trauma.
+          {"\ "}{translate('howToLook.paragraph4')}
         </Text>
     </View>
     );
