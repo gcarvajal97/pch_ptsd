8a204c41030ad034e86fb87106ed8443d4a28908
leahtbriggs <leahtbriggs@gmail.com>
2020-02-21 09:16:58 -0800

    US279T327: Change video rendering implementation and test
    Change video rendering in the How To Talk page to let me test the spanish translated video. Also changed the testing to mock for the spanish video.

--- findHelpScreens/__test__/HowToTalk.test.js
+++ findHelpScreens/__test__/HowToTalk.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import i18n from "i18n-js";
+import React from "react";
+import renderer from "react-test-renderer";
 import HowToTalk from '../HowToTalk';
 
-import renderer from 'react-test-renderer';
 
-test('renders correctly', () => {
-    const tree = renderer.create(<HowToTalk/>).toJSON();
-    expect(tree).toMatchSnapshot();
-}) 
+describe("HowToTalk", () => {
+  let wrapper;
+    beforeEach(() => {
+        i18n.locale = "en";
+    });
+
+    test("renders correctly", () => {
+        const tree = renderer.create(<HowToTalk/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
