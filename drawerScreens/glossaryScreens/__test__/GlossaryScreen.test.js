0369525c190bdf43073a27e238ecfdceb3b360b3
Aaron <akhan727@gmail.com>
2020-02-22 02:55:22 -0700

    I improved some of my test files to include seeing if English and Spanish translations are rendered properly.

--- drawerScreens/glossaryScreens/__test__/GlossaryScreen.test.js
+++ drawerScreens/glossaryScreens/__test__/GlossaryScreen.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import GlossaryScreen from '../GlossaryScreen';
-
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import i18n from "i18n-js";
 
-test('renders correctly', () => {
-    const tree = renderer.create(<GlossaryScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
+configure({ adapter: new Adapter() });
+
+describe("GlossaryScreen", () => {
+    
+    let wrapper;
+
+    beforeEach(() => {
+        i18n.local = "en";
+    });
+
+    it('renders correctly', () => {
+        const tree = renderer.create(<GlossaryScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders correctly in Spanish', () => {
+        i18n.locale = 'es'
+        const tree = renderer.create(<GlossaryScreen />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
