ccf55f063f4e90884ea273fc180478e1cef9753d
Aaron <akhan727@gmail.com>
2020-02-28 02:24:43 -0700

    US335T341 - I've reached 100% coverage for statements, branches, and functions.

--- drawerScreens/resourceScreens/__test__/Resources.test.js
+++ drawerScreens/resourceScreens/__test__/Resources.test.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import Resources from '../Resources';
 import renderer from 'react-test-renderer';
-import Enzyme, { shallow, configure } from 'enzyme';
+//import { Text } from "react-native-elements";
+import { Linking } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+import Enzyme, { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import i18n from "i18n-js";
+import en from '../../../languages/en.json';
+import es from '../../../languages/es.json';
 
 configure({ adapter: new Adapter() });
+jest.mock('expo', () => ({
+    Linking: {
+        openURL: jest.fn()
+    }
+}));
 
 describe("Resources", () => {
 
-    beforeEach(() => {
-        i18n.local = "en";
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("clicks external link, opens correct website", () => {
+        const translation = JSON.stringify(en.resources.accordion);
+        const content = [translation];
+        const result = Resources(content);      
+        const wrapper = shallow(result);    
+        wrapper.find('Text').first().props().onPress();
+    })
+
+    it('renders properly in english', () => {
+        const translation = JSON.stringify(en.resources.accordion);
+        const content = [translation];
+        const result = Resources(content);
+        expect(result).toMatchSnapshot();
+    });
+
+    it('renders properly in spanish', () => {
+        const translation = JSON.stringify(es.resources.accordion);
+        const content = [translation];
+        const result = Resources(content);
+        expect(result).toMatchSnapshot();
     });
 
     it('renders one resource', () => {
@@ -64,4 +95,14 @@ describe("Resources", () => {
         const result = Resources(undefined);
         expect(result).toMatchSnapshot();
     });
+});
+
+const styles = StyleSheet.create({
+    link: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#2089DC",
+        textDecorationLine: "underline",
+        marginVertical: 2
+    }
 });
