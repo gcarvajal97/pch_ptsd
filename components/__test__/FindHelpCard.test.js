4372812c53f4ddcd7822937c659708c098bcc03f
Anthony DeCosta <ardecost@asu.edu>
2020-02-29 19:51:22 -0800

    T344: Improved FindHelpCard's test coverage

--- components/__test__/FindHelpCard.test.js
+++ components/__test__/FindHelpCard.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import FindHelpCard from '../FindHelpCard';
+import NavigationService from '../../components/NavigationService';
 import renderer from 'react-test-renderer';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+jest.mock('../../components/NavigationService')
 
 describe("FindHelpCard", () => {
+
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
 
     it("matches Snapshot", () => {
         const snap = renderer.create(<FindHelpCard />).toJSON();
         expect(snap).toMatchSnapshot();
+    });
+
+    it("navigates to 'FindHelp' page on button press", () => {
+        const wrapper = shallow(<FindHelpCard />);
+        wrapper.findWhere((node) => node.prop('testID') === 'FindHelpButton').props().onPress();
+        expect(NavigationService.navigate).toHaveBeenCalledWith('FindHelp');
     });
 });
