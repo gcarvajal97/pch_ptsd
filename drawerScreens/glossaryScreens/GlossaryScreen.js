70c1e5c0c0e8f62114a352d2beb9517ef615c698
Anthony DeCosta <ardecost@asu.edu>
2020-04-07 13:51:09 -0700

    T422: Chg Button to rn-elements, update font

--- drawerScreens/glossaryScreens/GlossaryScreen.js
+++ drawerScreens/glossaryScreens/GlossaryScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, Platform, ScrollView, StyleSheet, View } from "react-native";
+import { Platform, ScrollView, StyleSheet, View } from "react-native";
+import { Button } from "react-native-elements";
 import NavigationService from "../../components/NavigationService";
 import PchPtsdAccordion from "../../components/shared/PchPtsdAccordion";
 import translate, { getLocale } from "../../components/translateService";
@@ -24,6 +25,7 @@ class GlossaryScreen extends Component {
             <Button
                 onPress={() => NavigationService.navigateDrawer("Home")}
                 title={translate("glossary.back")}
+                titleStyle={{ fontFamily: "avenir-medium"}}
                 color={colors.accent}
                 accessible
                 accessibilityLabel={translate("glossary.back")}
