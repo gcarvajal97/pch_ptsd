483662b6999162f09d2d262ed0f85520e38068dc
leahtbriggs <leahtbriggs@gmail.com>
2020-04-07 18:04:52 -0700

    Storyless: Test changes
    Test changes associated with color changes.

--- components/shared/__test__/PchPtsdAccordion.test.js
+++ components/shared/__test__/PchPtsdAccordion.test.js
@@ -1,6 +1,12 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import PchPtsdAccordion from "../PchPtsdAccordion";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import * as Animatable from "react-native-animatable";
+
+configure({ adapter: new Adapter() });
 
 describe("PchPtsdAccordion", () => {
     it("renders single section correctly", () => {
@@ -64,5 +70,55 @@ describe("PchPtsdAccordion", () => {
                 )
                 .toJSON()
         ).toMatchSnapshot();
+    });
+
+    it("should render an active section properly", () => {
+        const section = [
+            {
+                name: "section name",
+                content: [
+                    {
+                        name: "content name",
+                        content: "some content"
+                    }
+                ]
+            }
+        ];
+        const renderFn = props => {
+            `${props.name}${props.content}`;
+        };
+        const fixture = new PchPtsdAccordion({
+            sections: section,
+            contentRenderFunction: renderFn
+        });
+        expect(
+            renderer.create(fixture._renderHeader(section, null, true))
+        ).toMatchSnapshot();
+    });
+
+    it("should update active sections in state", () => {
+        const section = [
+            {
+                name: "section name",
+                content: [
+                    {
+                        name: "content name",
+                        content: "some content",
+                        accessibilityHint: "a hint"
+                    }
+                ]
+            }
+        ];
+        const renderFn = props => {
+            "this doesn't matter";
+        };
+        const instance = shallow(
+            <PchPtsdAccordion
+                sections={section}
+                contentRenderFunction={renderFn}
+            />
+        ).instance();
+        instance._updateSections([1]);
+        expect(instance.state.activeSections).toEqual([1]);
     });
 });
